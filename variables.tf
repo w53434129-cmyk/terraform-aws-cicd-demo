e0489436218aac2d6915df8ea97ac434905f49ab
w53434129-cmyk <w53434129@gmail.com>
2026-02-26 13:00:43 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
 
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name"
+  default     = "cicd-demo-bucket-2026"
+}
