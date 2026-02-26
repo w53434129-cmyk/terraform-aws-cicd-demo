555418cac141fb68125380a8af99c9cdb22b1ae1
w53434129-cmyk <w53434129@gmail.com>
2026-02-26 13:01:48 +0500

    Configure Terraform backend with S3 and DynamoDB

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-cicd-demo"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-cicd-demo"
+  }
+}
