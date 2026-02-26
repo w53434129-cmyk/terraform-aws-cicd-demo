4c4be687b3f2c610d31a3d3c0d4d9a16fa14d9c6
w53434129-cmyk <w53434129@gmail.com>
2026-02-26 13:00:22 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
 
+provider "aws" {
+  region = var.aws_region
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami           = "ami-0c94855ba95c71c99"  # Amazon Linux 2 AMI
+  instance_type = var.ec2_instance_type
+  tags = {
+    Name = "cicd-demo-web"
+  }
+}
+
+# S3 bucket
+resource "aws_s3_bucket" "data" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+}
