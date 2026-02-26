37fb6279f7ef2512f51da7fd4da5f4fe1f557099
w53434129-cmyk <w53434129@gmail.com>
2026-02-26 14:23:57 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-cicd-demo"
+    bucket         = "terraform-state-cicd-demo-warda-2026"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-cicd-demo"
