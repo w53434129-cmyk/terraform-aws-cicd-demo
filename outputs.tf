727260670ac6513736a8fc0dd373321f5609dffb
w53434129-cmyk <w53434129@gmail.com>
2026-02-26 15:11:20 +0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.main.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
+}
+
 output "ec2_instance_id" {
   value = aws_instance.web.id
 }
